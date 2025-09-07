298d67fa04d189fa26f951176e26a1d7d30ecad9
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2025-09-07 16:25:56 -0300

    Corrige o arquivo base.html

--- finans/settings.py
+++ finans/settings.py
@@ -105,11 +105,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]    
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILE_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static')
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
